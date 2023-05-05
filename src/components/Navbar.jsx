e1edb6543b82a78e3ccd0812714103365907f7a4
Adnan Ali <adnanali6688@gmail.com>
2023-05-05 17:54:44 +0200

    created navbar for big screens

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,50 @@
-import React from 'react'
+import React from "react";
+import { useState, useEffect } from "react";
+
+import { styles } from "../styles";
+import { navLinks } from "../constants";
+import { logo, menu, close } from "../assets";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
-  return (
-    <div>Navbar</div>
-  )
-}
+  const [active, setActive] = useState("");
+  const [activeLink, setActiveLink] = useState();
 
-export default Navbar
+  const moveToTop = () => {
+    setActive(window.scrollTo(0, 0));
+  };
+
+  const activateLink = (title) => {
+    setActiveLink(title);
+  };
+
+  return (
+    <nav
+      className={`${styles.paddingX} w-full flex items-center py-5 fixed top-0 z-20 bg-primary`}
+    >
+      <div className="w-full flex justify-between items-center max-w-7xl mx-auto">
+        <Link to="/" className="flex items-center gap-2" onClick={moveToTop}>
+          <img src={logo} alt="logo" className="w-9 h-9 object-contain" />
+          <p className="text-white text-[18px] font-bold cursor-pointer">
+            Adnan Ali
+          </p>
+        </Link>
+        <ul className="list-none hidden sm:flex flex-row gap-10">
+          {navLinks.map((link) => (
+            <li
+              key={link.id}
+              className={`${
+                activeLink === link.title ? "text-white" : "text-secondary"
+              } hover:text-white text-[18px] font-medium cursor-pointer`}
+              onClick={() => activateLink(link.title)}
+            >
+              <a href={`#${link.id}`}>{link.title}</a>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
