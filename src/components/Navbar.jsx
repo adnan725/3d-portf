a09fdfd1ec0f3104859605d076c9f442ded0bb06
Adnan Ali <adnanali6688@gmail.com>
2023-05-05 18:10:23 +0200

    created navbar for small screens

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,6 +9,7 @@ import { Link } from "react-router-dom";
 const Navbar = () => {
   const [active, setActive] = useState("");
   const [activeLink, setActiveLink] = useState();
+  const [toggle, setToggle] = useState(true);
 
   const moveToTop = () => {
     setActive(window.scrollTo(0, 0));
@@ -16,6 +17,11 @@ const Navbar = () => {
 
   const activateLink = (title) => {
     setActiveLink(title);
+    setToggle(!toggle);
+  };
+
+  const toggleIcon = () => {
+    setToggle(!toggle);
   };
 
   return (
@@ -42,6 +48,33 @@ const Navbar = () => {
             </li>
           ))}
         </ul>
+        <div className="sm:hidden flex flex-1 justify-end items-center">
+          <img
+            src={toggle ? menu : close}
+            alt="menu"
+            className="w-[28px] h-[28px] object-contain cursor-pointer"
+            onClick={toggleIcon}
+          />
+          <div
+            className={`${
+              toggle ? "hidden" : "flex"
+            } p-6 black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] z-10 rounded-xl`}
+          >
+            <ul className="list-none flex justify-end items-start flex-col gap-4">
+              {navLinks.map((link) => (
+                <li
+                  key={link.id}
+                  className={`${
+                    activeLink === link.title ? "text-white" : "text-secondary"
+                  } font-poppins text-[16px] font-medium cursor-pointer`}
+                  onClick={() => activateLink(link.title)}
+                >
+                  <a href={`#${link.id}`}>{link.title}</a>
+                </li>
+              ))}
+            </ul>
+          </div>
+        </div>
       </div>
     </nav>
   );
